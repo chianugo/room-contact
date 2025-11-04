6087aa383d4415d7a6d72d3ba59928fa187bce17
chianugo <chianugo.ononuju@babbangona.com>
2025-11-04 17:47:15 +0100

    Create contact event

--- app/src/main/java/com/example/roomcontact/ContactDao.kt
+++ app/src/main/java/com/example/roomcontact/ContactDao.kt
@@ -17,4 +17,12 @@ interface ContactDao {
     @Query("SELECT * FROM contact ORDER BY firstName ASC")
     //ascending order
     fun getContactsOrderedByFirstName(): Flow<List<Contact>>
+
+    @Query("SELECT * FROM contact ORDER BY lastName ASC")
+    //ascending order
+    fun getContactsOrderedByLastName(): Flow<List<Contact>>
+
+    @Query("SELECT * FROM contact ORDER BY phoneNumber ASC")
+    //ascending order
+    fun getContactsOrderedByPhoneNumber(): Flow<List<Contact>>
 }

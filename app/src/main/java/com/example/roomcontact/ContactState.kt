38703a634d6703940d62b33866078623ca1e9aa8
chianugo <chianugo.ononuju@babbangona.com>
2025-11-04 18:34:35 +0100

    Add contact view model

--- app/src/main/java/com/example/roomcontact/ContactState.kt
+++ app/src/main/java/com/example/roomcontact/ContactState.kt
@@ -2,9 +2,9 @@ package com.example.roomcontact
 
 data class ContactState(
     val contacts: List<Contact> = emptyList(),
-    val firstName: String,
-    val lastName: String,
-    val phoneNumber: String,
+    val firstName: String = "",
+    val lastName: String = "",
+    val phoneNumber: String = "",
     val isAddingContact: Boolean = false,
     val sortType: SortType = SortType.FIRST_NAME
 )

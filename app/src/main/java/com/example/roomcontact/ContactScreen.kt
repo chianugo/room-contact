be60aa71e31427c59136c889b979c6d3cc30b5f6
chianugo <chianugo.ononuju@babbangona.com>
2025-11-05 17:52:55 +0100

    Add save contact dialog

--- app/src/main/java/com/example/roomcontact/ContactScreen.kt
+++ app/src/main/java/com/example/roomcontact/ContactScreen.kt
@@ -3,15 +3,19 @@ package com.example.roomcontact
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -19,6 +23,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun ContactScreen(
@@ -59,6 +64,33 @@ fun ContactScreen(
 
                             Text(text = sortType.name)
                         }
+                    }
+                }
+            }
+            items(state.contacts) { contact ->
+                Row(modifier = Modifier.fillMaxWidth()) {
+                    Column(
+                        modifier = Modifier.weight(1f)
+                    ) {
+                        Text(
+                            text = "${contact.firstName} ${contact.lastName}",
+                            fontSize = 20.sp
+                        )
+
+                        Text(
+                            text = contact.phoneNumber,
+                            fontSize = 12.sp
+                        )
+                    }
+                    IconButton(
+                        onClick = {
+                            onEvent(ContactEvent.DeleteContact(contact))
+                        }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Delete,
+                            contentDescription = "DeleteContact"
+                        )
                     }
                 }
             }

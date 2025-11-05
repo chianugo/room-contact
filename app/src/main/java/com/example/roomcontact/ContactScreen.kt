39eeeb162c93c7e032bcedb58767c09bfa2e4fba
chianugo <chianugo.ononuju@babbangona.com>
2025-11-05 17:02:10 +0100

    Add contact screen

---
+++ app/src/main/java/com/example/roomcontact/ContactScreen.kt
@@ -0,0 +1,67 @@
+package com.example.roomcontact
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ContactScreen(
+    state: ContactState,
+    onEvent: (ContactEvent) -> Unit
+) {
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(onClick = { onEvent(ContactEvent.ShowDialog) }) {
+                Icon(
+                    imageVector = Icons.Default.AddCircle,
+                    contentDescription = "Add Contact"
+                )
+            }
+        }
+    ) { padding ->
+        LazyColumn(
+            contentPadding = padding,
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            item {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .horizontalScroll(rememberScrollState()),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    SortType.values().forEach { sortType ->
+                        Row(
+                            modifier = Modifier.clickable {
+                                onEvent(ContactEvent.SortContacts(sortType))
+                            }, verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            RadioButton(
+                                selected = state.sortType == sortType,
+                                onClick = { onEvent(ContactEvent.SortContacts(sortType)) })
+
+                            Text(text = sortType.name)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
